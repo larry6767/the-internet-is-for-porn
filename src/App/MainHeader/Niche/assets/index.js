8dc13be10a4bf2402689c48a60c98679fbcac797
Larry Glebov <larry.glebov@gmail.com>
2018-11-13 19:56:02 +0100

    refactored styles for __Niche--

---
+++ src/App/MainHeader/Niche/assets/index.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const NicheBlock = styled.div`
+    flex-grow: 1;
+
+    ${({theme}) => theme.media.sm`flex-grow: 0;`}
+    ${({theme}) => theme.media.xs`width: 100%;`}
+`
+
+export const NicheWrapper = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: flex-end;
+
+    ${({theme}) => theme.media.xs`display: none;`}
+`
+
+export const NicheMobile = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+`
+export const NicheMobileItem = styled.div`
+    width: 33.33%;
+    padding: 10px 0;
+    border: 2px solid ${({theme}) => theme.colors.mainColor2};
+    text-align: center;
+    color: ${({theme}) => theme.colors.additionalColor};
+
+    &:first-child {
+        border-radius: 4px 0 0 4px;
+        border-right: none;
+    }
+
+    &:last-child {
+        border-radius: 0 4px 4px 0;
+        border-left: none;
+    }
+`
+
+export const NicheMobileItemSelected = styled(NicheMobileItem)`
+    color: ${({theme}) => theme.colors.mainOppositeColor};
+`
+
+export const TextIcon = styled.div`
+    margin: 0 5px 0 0;
+    width: 24px;
+    height: 24px;
+    font-size: 24px;
+`
--- src/App/MainHeader/Search/assets/index.js
+++ src/App/MainHeader/Search/assets/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Search = styled.header`
+export const Search = styled.div`
     position: relative;
     width: 100%;
 
@@ -38,7 +38,7 @@ export const Search = styled.header`
 //     }
 // `
 
-export const SearchButton = styled.header`
+export const SearchButton = styled.button`
     position: absolute;
     top: 0;
     right: 0;
