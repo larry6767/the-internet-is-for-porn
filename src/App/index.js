3198661e4f8e7a4aba4ac0ae0b8c851215b9ab82
Larry Glebov <larry.glebov@gmail.com>
2018-12-05 17:23:25 +0100

    fixed breakpoints

--- src/App/index.js
+++ src/App/index.js
@@ -48,7 +48,7 @@ export default compose(
         }),
         dispatch => ({
             resizeAction: event => dispatch(actions.resize(
-                typeof event === "number" ? event : event.srcElement.innerWidth
+                typeof event === "number" ? event : event.srcElement.outerWidth
             ))
         })
     ),
