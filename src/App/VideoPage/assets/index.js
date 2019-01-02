fbf51d6e91dbbbd3c679936a4c0279aabb71cfce
Larry Glebov <larry.glebov@gmail.com>
2019-01-02 15:21:57 +0100

    VideoPage: added mobile version

--- src/App/VideoPage/assets/index.js
+++ src/App/VideoPage/assets/index.js
@@ -13,6 +13,10 @@ export const VideoPlayer = styled.div`
     margin-bottom: 60px;
     border-radius: 2px;
     overflow: hidden;
+    background-color: ${({theme}) => theme.colors.mainColor2};
+
+    ${({theme}) => theme.media.sm`flex-direction: column;`}
+    ${({theme}) => theme.media.mobile`flex-direction: column;`}
 `
 
 export const Video = styled.div`
@@ -20,16 +24,19 @@ export const Video = styled.div`
     display: flex;
     width: auto;
     flex-grow: 1;
-    padding: 10px;
-    background-color: ${({theme}) => theme.colors.mainColor2};
+    margin: 10px;
 
     &::before {
         display: block;
         content: '';
         padding-top: 60%;
+
+        ${({theme}) => theme.media.mobile`padding-top: 100%;`}
     }
 
     & iframe {
+        position: absolute;
+        z-index: 1;
         width: 100%;
         height: 100%;
         flex-grow: 1;
@@ -39,6 +46,7 @@ export const Video = styled.div`
 
 export const InlineAdvertisementWrapper = styled.div`
     position: absolute;
+    z-index: 2;
     top: 0;
     left: 0;
     right: 0;
@@ -55,6 +63,13 @@ export const InlineAdvertisement = styled.div`
     height: 254px;
     background-color: ${({theme}) => theme.colors.mainColor2};
 
+    ${({theme}) => theme.media.mobile`
+        top: calc(50% - 85px);
+        left: calc(50% - 100px);
+        width: 200px;
+        height: 170px;
+    `}
+
     & iframe {
         width: 100%;
         height: 100%;
@@ -63,6 +78,7 @@ export const InlineAdvertisement = styled.div`
 
 export const CloseAdvertisement = styled.div`
     position: absolute;
+    z-index: 2;
     top: 0;
     right: 0;
     width: 17px;
@@ -75,7 +91,20 @@ export const Advertisement = styled.div`
     width: 320px;
     padding: 10px;
     flex-direction: column;
-    background-color: ${({theme}) => theme.colors.mainColor2};
+
+    ${({theme}) => theme.media.sm`
+        width: 100%;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-around;
+    `}
+
+    ${({theme}) => theme.media.mobile`
+        width: 100%;
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: space-around;
+    `}
 
     & iframe {
         width: 300px;
@@ -85,6 +114,8 @@ export const Advertisement = styled.div`
 
     & iframe:not(:last-child) {
         margin-bottom: 10px;
+
+        ${({theme}) => theme.media.sm`margin-bottom: 0;`}
     }
 `
 
@@ -100,9 +131,17 @@ export const BottomAdvertisement = styled.section`
     padding: 10px 10px 7px;
     border-radius: 2px;
 
+    ${({theme}) => theme.media.sm`flex-wrap: wrap; justify-content: space-around;`}
+    ${({theme}) => theme.media.mobile`flex-wrap: wrap; justify-content: space-around;`}
+
     & iframe {
         width: 300px;
         height: 254px;
         border-radius: 2px;
+
+        &:not(:last-child) {
+            ${({theme}) => theme.media.sm`margin-bottom: 10px;`}
+            ${({theme}) => theme.media.mobile`margin-bottom: 10px;`}
+        }
     }
 `
