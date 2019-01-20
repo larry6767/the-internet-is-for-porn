bfa89b4e091e383d32d9bbf136fa1a0ac1f66b00
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-01-20 18:38:19 +0300

    small fix in comment for `App/helpers/getSubPage`

--- src/App/helpers/getSubPage.js
+++ src/App/helpers/getSubPage.js
@@ -12,9 +12,9 @@ const
     }),
 
     // API accepts requests like '/somepage-latest-5.html',
-    // but on the client side this is implemented like '/section/somepage?sort=lates&page=5'.
+    // but on the client side this is implemented like '/section/somepage?sort=latest&page=5'.
     getSubPage = (child, sort = null, page = 1, archive = []) => {
-        child = child || '' // Empty `child` is for pages like /somepage?sort=lates&page=5
+        child = child || '' // Empty `child` is for pages like /somepage?sort=latest&page=5
 
         switch (archive.length) {
             case 0: break;
