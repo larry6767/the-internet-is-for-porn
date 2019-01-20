7173692a1829e4ca2e0f7bffabc80d22eeab3c1d
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-01-21 01:00:35 +0300

    refactored `ssr/lib/legacy-redirects`

--- ssr/lib/legacy-redirects.jsx
+++ ssr/lib/legacy-redirects.jsx
@@ -14,92 +14,83 @@ const
             ext = ig(r, 'router', 'redirects', 'video', 'fromExt')
 
         return `/${videoPfx}:child/:name${ext}`
-    }
+    },
+
+    getFrom = (r, orientationCode, section, field = 'from') =>
+        ig(r, 'legacyOrientationPrefixes', orientationCode) +
+        ig(r, 'router', 'redirects', section, field)
 
 const
     // TODO redirects supposed to be splitted for specific orientations (see mapping module),
     //      see page codes mapping in backend-api mapping module to see how it could be done.
     LegacyRedirectsRouterBuilder = ({routerContext: r}) => <Switch>
-        {orientationCodes.map(orientationCode => [
-            <Route
-                key={`${orientationCode}-allNiches`}
-                exact
-                path={`${ig(r, 'legacyOrientationPrefixes', orientationCode)}/`}
-                render={props =>
-                    (
-                        ig(r, 'location', 'search') !==
-                        ig(r, 'router', 'redirects', 'categories', 'search')
-                    ) ? null : <Redirect
-                        to={routerGetters.allNiches.link(
-                            r.set('currentOrientation', orientationCode)
-                        )}
-                    />
-                }
-            />,
+        {orientationCodes.map(orientationCode => {
+            const
+                orientedR = r.set('currentOrientation', orientationCode)
 
-            <Redirect
-                key={`${orientationCode}-allMovies`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    ig(r, 'router', 'redirects', 'allMovies', 'from')
-                }
-                to={routerGetters.allMovies.link(r.set('currentOrientation', orientationCode))}
-            />,
+            return [
+                <Route
+                    key={`${orientationCode}-allNiches`}
+                    exact
+                    path={`${ig(r, 'legacyOrientationPrefixes', orientationCode)}/`}
+                    render={props =>
+                        (
+                            ig(r, 'location', 'search') !==
+                            ig(r, 'router', 'redirects', 'categories', 'search')
+                        ) ? null : <Redirect
+                            to={routerGetters.allNiches.link(orientedR)}
+                        />
+                    }
+                />,
 
-            <Redirect
-                key={`${orientationCode}-pornstars-redirect`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    ig(r, 'router', 'redirects', 'pornstars', 'from')
-                }
-                to={routerGetters.pornstars.link(r.set('currentOrientation', orientationCode))}
-            />,
+                <Redirect
+                    key={`${orientationCode}-allMovies`}
+                    exact
+                    from={getFrom(r, orientationCode, 'allMovies')}
+                    to={routerGetters.allMovies.link(orientedR)}
+                />,
 
-            <Redirect
-                key={`${orientationCode}-video-redirect`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    videoRedirectFrom(r)
-                }
-                to={
-                    // it must be `path` here, not `link` (to automatically fill the route masks)!
-                    routerGetters.video.path(r, orientationCode)
-                }
-            />,
+                <Redirect
+                    key={`${orientationCode}-pornstars-redirect`}
+                    exact
+                    from={getFrom(r, orientationCode, 'pornstars')}
+                    to={routerGetters.pornstars.link(orientedR)}
+                />,
 
-            <Redirect
-                key={`${orientationCode}-favorite`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    ig(r, 'router', 'redirects', 'favorite', 'from')
-                }
-                to={routerGetters.favorite.link(r.set('currentOrientation', orientationCode))}
-            />,
-            <Redirect
-                key={`${orientationCode}-favorite-movies`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    ig(r, 'router', 'redirects', 'favorite', 'fromMovies')
-                }
-                to={routerGetters.favorite.link(r.set('currentOrientation', orientationCode))}
-            />,
-            <Redirect
-                key={`${orientationCode}-favorite-pornstars`}
-                exact
-                from={
-                    ig(r, 'legacyOrientationPrefixes', orientationCode) +
-                    ig(r, 'router', 'redirects', 'favoritePornstars', 'from')
-                }
-                to={routerGetters.favoritePornstars.link(
-                    r.set('currentOrientation', orientationCode)
-                )}
-            />,
-        ])}
+                <Redirect
+                    key={`${orientationCode}-video-redirect`}
+                    exact
+                    from={
+                        ig(r, 'legacyOrientationPrefixes', orientationCode) +
+                        videoRedirectFrom(r)
+                    }
+                    to={
+                        // It must be `path` here, not `link`
+                        // (to automatically fill the route masks)!
+                        routerGetters.video.path(r, orientationCode)
+                    }
+                />,
+
+                <Redirect
+                    key={`${orientationCode}-favorite`}
+                    exact
+                    from={getFrom(r, orientationCode, 'favorite')}
+                    to={routerGetters.favorite.link(orientedR)}
+                />,
+                <Redirect
+                    key={`${orientationCode}-favorite-movies`}
+                    exact
+                    from={getFrom(r, orientationCode, 'favorite', 'fromMovies')}
+                    to={routerGetters.favorite.link(orientedR)}
+                />,
+                <Redirect
+                    key={`${orientationCode}-favorite-pornstars`}
+                    exact
+                    from={getFrom(r, orientationCode, 'favoritePornstars')}
+                    to={routerGetters.favoritePornstars.link(orientedR)}
+                />,
+            ]
+        })}
     </Switch>
 
 export default compose(
