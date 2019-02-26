92d2f998323786e4b59862f1ea62ca3eb830f2fa
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-02-27 01:36:12 +0300

    no checks in `plainProvedGet` and `immutableProvedGet` for production

--- src/App/helpers/plain/provedGet.js
+++ src/App/helpers/plain/provedGet.js
@@ -10,17 +10,30 @@ import {get} from 'lodash'
 // Also you could use it to verify that a value isn't an `undefined` like that:
 //   provedGet(varName, [])
 export default (obj, path, ...restPath) => {
-    if (restPath.length > 0)
-        path = [path].concat(restPath)
+    if (process.env.NODE_ENV === 'production') {
 
-    const result = path.length === 0 ? obj : get(obj, path)
+        if (restPath.length > 0) {
+            restPath.unshift(path)
+            return get(obj, restPath)
+        } else if (path.length === 0)
+            return obj
+        else
+            return get(obj, path)
 
-    if (result === undefined)
-        throw new Error(
-            `Failed to obtain a value by this path: ${JSON.stringify(path)} ` +
-            `(provided source object type: ${typeof obj}, `+
-            `top-level available keys: ${JSON.stringify(obj ? Object.keys(obj) : null)})`
-        )
-    else
-        return result
+    } else { // for development
+
+        if (restPath.length > 0)
+            path = [path].concat(restPath)
+
+        const result = path.length === 0 ? obj : get(obj, path)
+
+        if (result === undefined)
+            throw new Error(
+                `Failed to obtain a value by this path: ${JSON.stringify(path)} ` +
+                `(provided source object type: ${typeof obj}, `+
+                `top-level available keys: ${JSON.stringify(obj ? Object.keys(obj) : null)})`
+            )
+        else
+            return result
+    }
 }
