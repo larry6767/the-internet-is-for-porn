80dd3ab8e67dc7393a51ee60d824629d764362c4
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2019-02-26 23:31:38 +0300

    refactoring of __VideoItem__ thumbs

--- src/generic/VideoItem/index.js
+++ src/generic/VideoItem/index.js
@@ -1,5 +1,14 @@
-import React, {Component, Fragment} from 'react'
-import {compose, onlyUpdateForKeys, withPropsOnChange} from 'recompose'
+import React from 'react'
+
+import {
+    compose,
+    onlyUpdateForKeys,
+    withPropsOnChange,
+    withState,
+    withHandlers,
+    lifecycle,
+} from 'recompose'
+
 import {replace, set} from 'lodash'
 import {withStyles} from '@material-ui/core/styles'
 import {Typography} from '@material-ui/core'
@@ -33,72 +42,7 @@ import {
     TagsBlock,
     Like,
     Duration,
-    // VideoPreviewThumbs,
-    // LoadingProgress,
 } from './assets'
-
-// class PreviewThumbs extends Component {
-//     state = {
-//         currentThumb: 0,
-//         showed: false,
-//     }
-
-//     mouseEnter = () => {
-//         // preloading images
-//         this.props.thumbsLinks.map(x => set(new Image(), 'src', x)).toArray()
-
-//         // reset old timers in case they for some reason exists
-//         clearInterval(this.timeoutTimer)
-//         clearInterval(this.intervalTimer)
-
-//         this.timeoutTimer = setTimeout(() => {
-//             this.intervalTimer = setInterval(g(this, 'progress'), 500)
-//             this.setState({showed: true})
-//         }, 1000)
-//     }
-
-//     mouseLeave = () => {
-//         this.stopProgress()
-//     }
-
-//     componentWillUnmount() {
-//         this.stopProgress()
-//     }
-
-//     stopProgress = () => {
-//         clearInterval(this.timeoutTimer)
-//         clearInterval(this.intervalTimer)
-
-//         this.setState({
-//             currentThumb: 0,
-//             showed: false,
-//         })
-//     }
-
-//     progress = () => {
-//         const
-//             currentThumb = g(this, 'state', 'currentThumb'),
-//             thumbsLinks = g(this, 'props', 'thumbsLinks')
-
-//         this.setState({
-//             currentThumb: g(thumbsLinks, 'size') - 1 > currentThumb ? currentThumb + 1 : 0
-//         })
-//     }
-
-//     render() {
-//         const
-//             thumbsLinks = g(this, 'props', 'thumbsLinks')
-
-//         return <VideoPreviewThumbs
-//             onMouseEnter={g(this, 'mouseEnter')}
-//             onMouseLeave={g(this, 'mouseLeave')}
-//             thumb={ig(thumbsLinks, g(this, 'state', 'currentThumb'))}
-//             showed={g(this, 'state', 'showed')}
-//         >
-//             {renderLink(g(this.props, 'x'), g(this.props, 'getVideoLink'))}
-//         </VideoPreviewThumbs>
-//     }
-// }
 
 const
     renderLink = (x, getVideoLink) => typeof ig(x, 'videoPageRef') === 'string' // external resource
@@ -118,21 +62,110 @@ const
         </Typography>
     </ProviderLink>,
 
-    renderVideoPreview = (
-        classes, x, cb, isSSR, addVideoToFavoriteHandler,
-        removeVideoFromFavoriteHandler, isThisVideoFavorite, thumbsLinks,
-        getSearchLink, getVideoLink, videoPreviewStyle,
-    ) => <VideoPreview style={videoPreviewStyle}>
-        {/* {isSSR ? null : <Fragment>
-            <LoadingProgress/>
-            <PreviewThumbs
-                thumbsLinks={thumbsLinks}
-                classes={classes}
-                x={x}
-                getVideoLink={getVideoLink}
-            />
-        </Fragment>} */}
+    VideoPreviewRender = compose(
+        withPropsOnChange(['x'], props => {
+            const
+                thumbsCount = g(ig(props.x, 'thumbs'), 'size')
 
+            return {
+                // WARNING! These are mutable to avoid triggering state change events.
+                //          For internal usage only (inside `compose`d HOCs).
+                mutableTimersIds: thumbsCount === 0 ? null : {
+                    timeout: null,
+                    interval: null,
+                },
+
+                thumbsCount,
+            }
+        }),
+        withState('currentThumb', 'setCurrentThumb', null),
+        withState('thumbsArePreloaded', 'setThumbsArePreloaded', false),
+        withHandlers({
+            progress: props => () => {
+                const
+                    currentThumb = g(props, 'currentThumb')
+
+                props.setCurrentThumb(
+                    g(props, 'thumbsCount') - 1 > currentThumb ? currentThumb + 1 : 0
+                )
+            },
+
+            stopProgress: props => () => {
+                clearTimeout(g(props, 'mutableTimersIds', 'timeout'))
+                clearInterval(g(props, 'mutableTimersIds', 'interval'))
+                props.setCurrentThumb(null)
+            },
+        }),
+        withHandlers({
+            mouseLeaveHandler: props => () => {
+                if (g(props, 'thumbsCount') === 1)
+                    return
+
+                props.stopProgress()
+            },
+
+            mouseEnterHandler: props => () => {
+                if (g(props, 'thumbsCount') === 1)
+                    return
+
+                // preloading images
+                if ( ! g(props, 'thumbsArePreloaded')) {
+                    ig(props.x, 'thumbs')
+                        .map(thumb => set(
+                            new Image(),
+                            'src',
+                            replace(ig(props.x, 'thumbMask'), '{num}', thumb)
+                        ))
+                        .toArray()
+
+                    props.setThumbsArePreloaded(true)
+                }
+
+                // reset old timers in case they for some reason exists
+                clearTimeout(g(props, 'mutableTimersIds', 'timeout'))
+                clearInterval(g(props, 'mutableTimersIds', 'interval'))
+
+                g(props, 'mutableTimersIds').timeout = setTimeout(() => {
+                    props.setCurrentThumb(0)
+                    g(props, 'mutableTimersIds').interval = setInterval(g(props, 'progress'), 500)
+                }, 1000)
+            },
+        }),
+        lifecycle({
+            componentWillUnmount() {
+                if (g(this, 'props', 'thumbsCount') === 1)
+                    return
+
+                this.props.stopProgress()
+            },
+        }),
+        withPropsOnChange(['x', 'currentThumb'], props => {
+            const
+                currentThumb = g(props, 'currentThumb')
+
+            return {
+                videoPreviewStyle: Object.freeze({
+                    backgroundImage: `url(${
+                        currentThumb === null
+                            ? ig(props.x, 'thumb')
+                            : ig(props.x, 'thumbMask')
+                                .replace('{num}', ig(props.x, 'thumbs', currentThumb))
+                    })`,
+                }),
+            }
+        }),
+        onlyUpdateForKeys(['cb', 'x', 'isThisVideoFavorite', 'videoPreviewStyle'])
+    )(({
+        classes, x, cb, isSSR, addVideoToFavoriteHandler,
+        removeVideoFromFavoriteHandler, isThisVideoFavorite,
+        getSearchLink, getVideoLink,
+        videoPreviewStyle, mouseEnterHandler, mouseLeaveHandler, thumbsCount,
+    }) => <VideoPreview
+        onMouseEnter={g(mouseEnterHandler, [])}
+        onMouseLeave={g(mouseLeaveHandler, [])}
+        style={videoPreviewStyle}
+        hasOnlyOneThumb={thumbsCount === 1}
+    >
         {isSSR ? renderLink(x, getVideoLink) : null}
 
         <VideoPreviewBar>
@@ -159,42 +192,43 @@ const
                 </Typography>
             </Duration>
         </VideoPreviewBar>
-    </VideoPreview>,
+    </VideoPreview>),
 
     VideoItem = ({
         classes, x, cb, getVideoLink, getSearchLink, isSSR, addVideoToFavoriteHandler,
-        removeVideoFromFavoriteHandler, isThisVideoFavorite, videoPreviewStyle
-    }) => {
-        const
-            thumbsLinks = ig(x, 'thumbs').map(thumb => replace(ig(x, 'thumbMask'), '{num}', thumb))
+        removeVideoFromFavoriteHandler, isThisVideoFavorite,
+    }) => <Wrapper>
+        <VideoPreviewRender
+            classes={classes}
+            x={x}
+            cb={cb}
+            isSSR={isSSR}
+            addVideoToFavoriteHandler={addVideoToFavoriteHandler}
+            removeVideoFromFavoriteHandler={removeVideoFromFavoriteHandler}
+            isThisVideoFavorite={isThisVideoFavorite}
+            getSearchLink={getSearchLink}
+            getVideoLink={getVideoLink}
+        />
 
-        return <Wrapper>
-            {renderVideoPreview(
-                classes, x, cb, isSSR, addVideoToFavoriteHandler,
-                removeVideoFromFavoriteHandler, isThisVideoFavorite, thumbsLinks,
-                getSearchLink, getVideoLink, videoPreviewStyle,
-            )}
+        <InfoBlock>
+            <Typography
+                variant="body1"
+                className={g(classes, 'typographyTitle')}
+                title={ig(x, 'title')}
+            >
+                {ig(x, 'title')}
+            </Typography>
+            <InfoBlockInner>
+                {ccb(cb, xs) === 0 ? null : renderProviderLink(classes, x, getSearchLink)}
 
-            <InfoBlock>
-                <Typography
-                    variant="body1"
-                    className={g(classes, 'typographyTitle')}
-                    title={ig(x, 'title')}
-                >
-                    {ig(x, 'title')}
-                </Typography>
-                <InfoBlockInner>
-                    {ccb(cb, xs) === 0 ? null : renderProviderLink(classes, x, getSearchLink)}
-
-                    <TagsBlock>
-                        {ig(x, 'tagsShort').map(x => <StyledLink key={x} to={getSearchLink(x)}>
-                            {x}
-                        </StyledLink>)}
-                    </TagsBlock>
-                </InfoBlockInner>
-            </InfoBlock>
-        </Wrapper>
-    }
+                <TagsBlock>
+                    {ig(x, 'tagsShort').map(x => <StyledLink key={x} to={getSearchLink(x)}>
+                        {x}
+                    </StyledLink>)}
+                </TagsBlock>
+            </InfoBlockInner>
+        </InfoBlock>
+    </Wrapper>
 
 export default compose(
     withStyles(muiStyles),
@@ -202,11 +236,6 @@ export default compose(
         isThisVideoFavorite: Boolean(
             g(props, 'favoriteVideoList').find(id => id === ig(g(props, 'x'), 'id'))
         ),
-    })),
-    withPropsOnChange(['x'], props => ({
-        videoPreviewStyle: Object.freeze({
-            backgroundImage: `url(${ig(props.x, 'thumb')})`,
-        }),
     })),
     onlyUpdateForKeys(['cb', 'x', 'isThisVideoFavorite']),
     setPropTypes(process.env.NODE_ENV === 'production' ? null : {
@@ -224,7 +253,6 @@ export default compose(
         cb: PropTypes.oneOf(breakpoints),
         x: immutableVideoItemModel,
         isThisVideoFavorite: PropTypes.bool,
-        videoPreviewStyle: PropTypes.object,
         favoriteVideoList: ImmutablePropTypes.listOf(PropTypes.number),
 
         addVideoToFavoriteHandler: PropTypes.func,
