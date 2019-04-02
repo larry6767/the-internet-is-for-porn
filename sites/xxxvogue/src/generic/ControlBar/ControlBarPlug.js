20c31db09690d1e9ccf8bcbff834f7d0a5a7f127
Larry Glebov <larry.glebov@gmail.com>
2019-04-02 16:21:46 +0200

    xxxvogue: __ControlBar__: changed type of idnex, ControlBarPlug

--- sites/xxxvogue/src/generic/ControlBar/ControlBarPlug.js
+++ sites/xxxvogue/src/generic/ControlBar/ControlBarPlug.js
@@ -1,1 +1,16 @@
-../../../../../shared/src/generic/ControlBar/ControlBarPlug.js
+import React from 'react'
+
+// local libs
+import {Wrapper, ControlButtons, SortPlug, ShowedElementsPlug} from 'src/generic/ControlBar/assets'
+import {PaginationPlug} from 'src/generic/Pagination/assets'
+
+const
+    ControlBarPlug = () => <Wrapper>
+        <ControlButtons>
+            <PaginationPlug/>
+            <SortPlug/>
+        </ControlButtons>
+        <ShowedElementsPlug/>
+    </Wrapper>
+
+export default ControlBarPlug
