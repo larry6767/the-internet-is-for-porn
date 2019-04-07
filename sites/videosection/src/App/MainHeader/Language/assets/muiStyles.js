2f253b6b3d7202be11ba698f275edbb66aceff1e
Larry Glebov <larry.glebov@gmail.com>
2019-04-07 12:32:10 +0200

    videosection: fixed language select

--- sites/videosection/src/App/MainHeader/Language/assets/muiStyles.js
+++ sites/videosection/src/App/MainHeader/Language/assets/muiStyles.js
@@ -3,8 +3,9 @@ export const muiStyles = {
         display: 'flex'
     },
     select: {
+        maxHeight: 50,
         paddingRight: 25,
-        paddingTop: 11.5,
+        paddingTop: 11,
         paddingBottom: 9,
         display: 'flex',
         alignItems: 'center'
